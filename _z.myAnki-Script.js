5086ca0be6302181955a1b1d9d9572c913276719
robertogp88 <139132384+robertogp88@users.noreply.github.com>
2026-08-23 11:15:09 +0200

    Delete _z.myAnki-Script.js

--- _z.myAnki-Script.js
+++
@@ -1,9 +0,0 @@
-    (function() {
-        // Seleccionamos todos los divs que contienen el texto sucio
-        var elementos = document.querySelectorAll('.contenido-limpio');
-        elementos.forEach(function(el) {
-            // Quitamos las etiquetas de cloze para que sea legible
-			// y envolvemos el texto del cloze en un span para apicar el estilo .cloze-limpio a los clozes inactivos
-            el.innerHTML = el.innerHTML.replace(/\{\{c\d+::(.+?)(?:::.+?)?\}\}/g, '<span class="cloze-limpio">$1</span>');
-        });
-    })();
