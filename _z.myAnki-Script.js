4e97af3e6dc597d750caf2bb18abed07d8370d22
robertogp88 <139132384+robertogp88@users.noreply.github.com>
2026-08-23 11:04:21 +0200

    Refactor cloze handling and remove scroll function
    Removed the scrollToCloze function and its invocation, and added functionality to clean up cloze tags in the content.

--- _z.myAnki-Script.js
+++ _z.myAnki-Script.js
@@ -1,17 +1,9 @@
-
-(function () {
-  function scrollToCloze() {
-    const cloze = document.querySelector('.cloze');
-    if (!cloze) return;
-
-    cloze.scrollIntoView({
-      behavior: 'auto',
-    //   block: 'center'
-      block: 'start'
-
-    });
-  }
-
-  // Espera a que Anki termine de renderizar
-  setTimeout(scrollToCloze, 0);
-})();
+    (function() {
+        // Seleccionamos todos los divs que contienen el texto sucio
+        var elementos = document.querySelectorAll('.contenido-limpio');
+        elementos.forEach(function(el) {
+            // Quitamos las etiquetas de cloze para que sea legible
+			// y envolvemos el texto del cloze en un span para apicar el estilo .cloze-limpio a los clozes inactivos
+            el.innerHTML = el.innerHTML.replace(/\{\{c\d+::(.+?)(?:::.+?)?\}\}/g, '<span class="cloze-limpio">$1</span>');
+        });
+    })();
