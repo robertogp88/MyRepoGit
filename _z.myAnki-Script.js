2e79ead011cefad7742a276299b5cafdc750cea6
robertogp88 <139132384+robertogp88@users.noreply.github.com>
2026-08-23 11:01:50 +0200

    Update scroll behavior and timing in myAnki-Script
    Change scroll behavior to center and adjust execution timing.

--- _z.myAnki-Script.js
+++ _z.myAnki-Script.js
@@ -8,12 +8,11 @@
 
     cloze.scrollIntoView({
       behavior: 'auto',
-    //   block: 'center'
-      block: 'start'
-
+      block: 'center'
     });
   }
 
-  // Espera a que Anki termine de renderizar
-  setTimeout(scrollToCloze, 0);
+  // Ejecutar de inmediato y por si acaso un reintento mínimo
+  scrollToCloze();
+  setTimeout(scrollToCloze, 50);
 })();
