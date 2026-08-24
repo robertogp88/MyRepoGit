a784efbd8d268fa9bb55464ccfc8bad8961fe660
robertogp88 <139132384+robertogp88@users.noreply.github.com>
2026-08-24 09:57:55 +0200

    Add new version of myAnki script

--- _z.myAnki-Script-V8.js
+++ _z.myAnki-Script-V8.js
@@ -97,24 +97,6 @@ document.addEventListener("DOMContentLoaded", () => {
 /* 🟩🟨🟩🟨🟩🟨 CLOZES - CLOZES - CLOZES 🟩🟨🟩🟨🟩🟨*/
 /* 🟩🟨🟩🟨🟩🟨 CLOZES - CLOZES - CLOZES 🟩🟨🟩🟨🟩🟨*/
 
-// PARA QUE AL MOSTRARSE LA PREGUNTA VAYA DIRECTAMENTE AL CLOZE ACTIVO
-
-(function () {
-  function scrollToCloze() {
-    const cloze = document.querySelector('.cloze');
-    if (!cloze) return;
-
-    cloze.scrollIntoView({
-      behavior: 'auto',
-    //   block: 'center'
-      block: 'start'
-
-    });
-  }
-
-  // Espera a que Anki termine de renderizar
-  setTimeout(scrollToCloze, 0);
-})();
 
 
 // MOSTRAR CLOZES INACTIVOS EN LOS CAMPOS FUERA DE REPASO 
